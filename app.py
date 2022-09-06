71e8e94be5068f64296d11515c1250b87285d4b8
slawa loev <avenger_of_darkness1989@yahoo.de>
2022-09-06 18:18:03 +0200

    brought the website design closer to amex corporate design, introduced config file

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ from pandas import read_csv
 import requests
 import json
 
-st.markdown("<h1 style='text-align: center; color: purple;'>Will your customer pay you BACK?!</h1>", unsafe_allow_html=True)
+#st.set_page_config(page_title="Monte Carlo Pi", page_icon='🔢',layout="wide")
 
-st.markdown("<h4 style='text-align: center; color: grey;'>Dear American Express managment, </h4>", unsafe_allow_html=True)
-st.markdown("<h5 style='text-align: center; color: grey;'>Please only upload a CSV file in the following format: </h5>", unsafe_allow_html=True)
+font1 = 'font-family: Helvetica;'
+font2 = 'font-family: sans-serif;'
+
+st.markdown(f"<h1 style='text-align: center;{font1} color: #006fcf;'>Will your credit card customer pay you back?</h1>", unsafe_allow_html=True)
+
+st.markdown(f"<h4 style='text-align: center;{font2} color: #334251;'>Dear American Express managment, </h4>", unsafe_allow_html=True)
+st.markdown(f"<h5 style='text-align: center;{font2} color: #334251;'>Please only upload a CSV file in the following format: </h5>", unsafe_allow_html=True)
 
 
 col1, col2, col3, col4, col5 = st.columns(5)
@@ -49,19 +54,19 @@ if submitted:
                                     params = params_user).json()
 
 
-        st.markdown("<h3 style='text-align: center; color: black;'>Thank you for submitting</h3>", unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: center; color: white;'>Thank you for submitting</h3>", unsafe_allow_html=True)
         st.write('-------------')
-        st.markdown("<h5 style='text-align: center; color: black;'>Customer with ID:</h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: center; color: white;'>Customer with ID:</h5>", unsafe_allow_html=True)
 
         if predictions['output'] == 'defaulter':
 
             html_str_custID = f"<h6 style='text-align: center; color: grey;'>{predictions['customer_ID']}</h6>"
             st.markdown(html_str_custID, unsafe_allow_html=True)
-            st.markdown("<h5 style='text-align: center; color: black;'>Must be a ... </h5>", unsafe_allow_html=True)
+            st.markdown("<h5 style='text-align: center; color: white;'>Must be a ... </h5>", unsafe_allow_html=True)
             html_str_output = f"<h3 style='text-align: center; color: red;'>{predictions['output']}</h3>"
             st.markdown(html_str_output, unsafe_allow_html=True)
-            st.markdown("<h6 style='text-align: center; color: black;'>With probability of:</h6>", unsafe_allow_html=True)
-            html_str_proba = f"<h3 style='text-align: center; color: red;'>{predictions['probability']}</h3>"
+            st.markdown("<h6 style='text-align: center; color: white;'>With probability of:</h6>", unsafe_allow_html=True)
+            html_str_proba = f"<h3 style='text-align: center; color: red;'>{float(predictions['probability'])*100} %</h3>"
             st.markdown(html_str_proba, unsafe_allow_html=True)
 
 
@@ -74,25 +79,6 @@ if submitted:
             st.markdown("<h6 style='text-align: center; color: black;'>With probability of:</h6>", unsafe_allow_html=True)
             html_str_proba = f"<h3 style='text-align: center; color: green;'>{float(predictions['probability'])*100} %</h3>"
             st.markdown(html_str_proba, unsafe_allow_html=True)
-
-        # p_2 = read_csv(uploaded_csv_file, index_col=0).fillna('').to_dict(orient='records')[0]['P_2']
-
-        # if p_2 == 0.3671699213:
-        #     st.markdown("<h6 style='text-align: center; color: grey;'>e5a854a3211e83db521500e8b70360fe9670af1df904017d84cf245c2ec1c68b</h6>", unsafe_allow_html=True)
-        #     st.markdown("<h5 style='text-align: center; color: black;'>Must be a ... </h5>", unsafe_allow_html=True)
-        #     st.markdown("<h5 style='text-align: center; color: green;'>payer</h5>", unsafe_allow_html=True)
-        #     st.markdown("<h6 style='text-align: center; color: black;'>With probability of:</h6>", unsafe_allow_html=True)
-        #     st.markdown("<h3 style='text-align: center; color: green;'>94.7 %</h3>", unsafe_allow_html=True)
-
-        # elif p_2 == 0.5243904932:
-        #     st.markdown("<h6 style='text-align: center; color: grey;'>6ba461c93869797c49b0f34c29274e50915466eda02a82fde4aa2c73c3924339</h6>", unsafe_allow_html=True)
-        #     st.markdown("<h5 style='text-align: center; color: black;'>Must be a ... </h5>", unsafe_allow_html=True)
-        #     st.markdown("<h5 style='text-align: center; color: red;'>defaulter</h5>", unsafe_allow_html=True)
-        #     st.markdown("<h6 style='text-align: center; color: black;'>With probability of:</h6>", unsafe_allow_html=True)
-        #     st.markdown("<h3 style='text-align: center; color: red;'>84.3 %</h3>", unsafe_allow_html=True)
-
-        # else:
-        #     st.markdown("<h5 style='text-align: center; color: red;'>Wrong submission, please upload CSV file in the right format</h5>", unsafe_allow_html=True)
 
 
 st.write('-------------')
@@ -109,7 +95,4 @@ st.write('\n\n\n\nThis wonderful predictor is brought to you by:')
 st.image([isis_img, slawa_img, yuzhe_img, sjoerd_img, lewagon_img],
          caption=['Isis!', 'Slawa!', 'Yuzhe!', 'Sjoerd!', 'Le Wagon!'], width=100)
 
-st.write('-------------')
-
-mlfoto = Image.open('team_imgs/MLfoto.webp')
-st.image(mlfoto)
+#st.write('-------------')
