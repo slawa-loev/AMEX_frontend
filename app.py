6f80b73f71b3d025ecf331896f70bed5525d2af5
Sjoerd de Wit <sjoerdreyer@gmail.com>
2022-09-14 17:46:07 +0200

    changes_website

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from PIL import Image
 from pandas import read_csv
 import requests
 import json
+from shap import shap
+import matplotlib.pyplot as plt
 
 
-
-#col1, col2, col3, col4, col5 = st.columns(5)
 
 st.set_page_config(page_title="AMEX Oracle", page_icon='',layout="wide")
 
@@ -25,15 +25,10 @@ with open( "style.css" ) as css:
     #st.markdown('AMEX ORACLE')
 
 
-col2, col4 = st.columns([8, 8])
-#col1, col2, col3, col4 = st.columns([1, 6, 1, 8])
-
-
-
-
+col1, col2 = st.columns([10, 8])
 
 amex_logo = Image.open('team_imgs/amex_oracle_logo.jpg')
-col2.image(amex_logo,
+col1.image(amex_logo,
            use_column_width=True,
            #width=350,
            #output_format='PNG'
@@ -45,35 +40,50 @@ col2.image(amex_logo,
 
 
 
-col4.markdown(f"<h4 style='text-align: center;{font5} {amexblue}'>Will your credit card customer pay you back?</h1>", unsafe_allow_html=True)
+col2.markdown(f"<h4 style='text-align: center;{font5} {amexblue}'>Will your credit card customer pay you back?</h4>", unsafe_allow_html=True)
 
 
 
 
-col4.markdown(f"<h5 style='text-align: center;{font2} color: #334251;'>Dear American Express managment, </h4>", unsafe_allow_html=True)
-col4.markdown(f"<h6 style='text-align: center;{font2} color: #334251;'>Please upload a CSV file in the following format: </h5>", unsafe_allow_html=True)
+col2.markdown(f"<h5 style='text-align: center;{font2} color: #334251;'>Dear American Express managment, </h5>", unsafe_allow_html=True)
+col2.markdown(f"<h6 style='text-align: center;{font2} color: #334251;'>Please upload a CSV file in the following format: </h6>", unsafe_allow_html=True)
+
+
+with open('template_csv/template.csv') as f:
+    if col2.download_button('Template', f, 'template.csv'):
+        col2.write('Thanks for downloading!')
 
 
 
-with open('template_csv/AMEX_client_template.csv') as f:
-    if col4.download_button('Example template', f, 'example_template.csv'):
-        col4.write('Thanks for downloading!')
+col2.markdown(f"<h6 style='text-align: left;{font2} color: #334251;'>Features should be anonymized and normalized, and fall into the following general categories: </h6>", unsafe_allow_html=True)
+col2.markdown(f"<h6 style='text-align: left;{font2} color: #334251;'>D_* = Delinquency variables </h6>", unsafe_allow_html=True)
+col2.markdown(f"<h6 style='text-align: left;{font2} color: #334251;'>S_* = Spend variables </h6>", unsafe_allow_html=True)
+col2.markdown(f"<h6 style='text-align: left;{font2} color: #334251;'>P_* = Payment variables </h6>", unsafe_allow_html=True)
+col2.markdown(f"<h6 style='text-align: left;{font2} color: #334251;'>B_* = Balance variables </h6>", unsafe_allow_html=True)
+col2.markdown(f"<h6 style='text-align: left;{font2} color: #334251;'>R_* = Risk variables </h6>", unsafe_allow_html=True)
 
-# with open('template_csv/Slawa_Loev.csv') as s:
-#     if col4.download_button('Slawa_new_customer', s, 'Slawa_Loev.csv'):
-#         col4.write('Thanks for downloading!')
-
-# with open('template_csv/Sjoerd_de_Wit.csv') as w:
-#     if col4.download_button('Sjoerd_new_customer', w, 'Sjoerd_de_Wit.csv'):
-#         col4.write('Thanks for downloading!')
+st.write('-------------')
 
 
 
-#st.write('\n\n-------------')
+
+st.markdown(f"<h6 style='text-align: center;{font2} color: #334251;'>You can use these examples to try out the ORACLE: </h6>", unsafe_allow_html=True)
 
 
+col3, col4, col5, col6 = st.columns([8, 5, 5, 8])
 
-with col4.form("my_form"):
+with open('template_csv/example_payer.csv') as f:
+    col4.download_button('Data example #1', f, 'data_example_1.csv')
+
+
+with open('template_csv/example_defaulter.csv') as s:
+    col5.download_button('Data example #2', s, 'data_example_2.csv')
+
+
+col7, col8, col9 = st.columns([1, 3, 1])
+
+
+with col8.form("my_form"):
 
     uploaded_csv_file = st.file_uploader("Upload a customer CSV File",
                                          type=['csv'],
@@ -102,42 +112,53 @@ if submitted:
         predictions = requests.get(url_api,
                                     params = params_user).json()
 
-        st.write('-------------')
-        st.markdown("<h4 style='text-align: center; color: black;'>Thank you for submitting</h4>", unsafe_allow_html=True)
-        st.write('-------------')
-        st.markdown("<h5 style='text-align: center; color: black;'>Potential customer:</h5>", unsafe_allow_html=True)
 
-        if predictions['output'] == 'defaulter':
-
-            html_str_custID = f"<h3 style='text-align: center; color: grey;'>{uploaded_csv_file.name[:-4].replace('_',' ')}</h3>"
-            st.markdown(html_str_custID, unsafe_allow_html=True)
-            st.markdown("<h5 style='text-align: center; color: black;'>will ... </h5>", unsafe_allow_html=True)
-            html_str_output = f"<h2 style='text-align: center; color: red;'>not pay you back</h2>"
-            st.markdown(html_str_output, unsafe_allow_html=True)
-            st.markdown("<h10 style='text-align: center; color: black;'> </h10>", unsafe_allow_html=True)
-            st.markdown("<h5 style='text-align: center; color: black;'>With probability of:</h5>", unsafe_allow_html=True)
-            html_str_proba = f"<h1 style='text-align: center; color: red;'>{float(predictions['probability'])*100} %</h1>"
-            st.markdown(html_str_proba, unsafe_allow_html=True)
-            col1,col2,col3 = st.columns([1,3,1])
-            col2.write('-------------')
-            col2.image(default_shap, use_column_width=True)
-            col2.write('-------------')
-
+        if predictions['customer_ID'] == 0:
+            st.markdown("<h5 style='text-align: center; color: red;'> Template does not give output, please upload a csv with data </h5>", unsafe_allow_html=True)
         else:
 
-            html_str_custID = f"<h3 style='text-align: center; color: grey;'>{uploaded_csv_file.name[:-4].replace('_',' ')}</h3>"
-            st.markdown(html_str_custID, unsafe_allow_html=True)
-            st.markdown("<h5 style='text-align: center; color: black;'>will ... </h5>", unsafe_allow_html=True)
-            html_str_output = f"<h2 style='text-align: center; color: green;'>pay you back</h2>"
-            st.markdown(html_str_output, unsafe_allow_html=True)
-            st.markdown("<h10 style='text-align: center; color: black;'> </h10>", unsafe_allow_html=True)
-            st.markdown("<h5 style='text-align: center; color: black;'>With probability of:</h5>", unsafe_allow_html=True)
-            html_str_proba = f"<h1 style='text-align: center; color: green;'>{float(predictions['probability'])*100} %</h1>"
-            st.markdown(html_str_proba, unsafe_allow_html=True)
-            col1,col2,col3 = st.columns([1,3,1])
-            col2.write('-------------')
-            col2.image(pay_shap, use_column_width=True)
-            col2.write('-------------')
+            st.write('-------------')
+            st.markdown("<h4 style='text-align: center; color: black;'>Thank you for submitting</h4>", unsafe_allow_html=True)
+            st.write('-------------')
+            st.markdown("<h5 style='text-align: center; color: black;'>Potential customer:</h5>", unsafe_allow_html=True)
+
+            if predictions['output'] == 'defaulter':
+
+                html_str_custID = f"<h6 style='text-align: center; color: grey;'>{predictions['customer_ID']}</h6>"
+                st.markdown(html_str_custID, unsafe_allow_html=True)
+                st.markdown("<h5 style='text-align: center; color: black;'>will ... </h5>", unsafe_allow_html=True)
+                html_str_output = f"<h2 style='text-align: center; color: red;'>not pay you back</h2>"
+                st.markdown(html_str_output, unsafe_allow_html=True)
+                st.markdown("<h10 style='text-align: center; color: black;'> </h10>", unsafe_allow_html=True)
+                st.markdown("<h5 style='text-align: center; color: black;'>With probability of:</h5>", unsafe_allow_html=True)
+                html_str_proba = f"<h1 style='text-align: center; color: red;'>{float(predictions['probability'])*100} %</h1>"
+                st.markdown(html_str_proba, unsafe_allow_html=True)
+                col1,col2,col3 = st.columns([1,3,1])
+                col2.write('-------------')
+                col2.image(default_shap, use_column_width=True)
+                col2.write('-------------')
+                shap.make_shap_plot(uploaded_csv_file)
+                st.pyplot(bbox_inches='tight')
+                plt.clf()
+
+            else:
+
+                html_str_custID = f"<h6 style='text-align: center; color: grey;'>{predictions['customer_ID']}</h6>"
+                st.markdown(html_str_custID, unsafe_allow_html=True)
+                st.markdown("<h5 style='text-align: center; color: black;'>will ... </h5>", unsafe_allow_html=True)
+                html_str_output = f"<h2 style='text-align: center; color: green;'>pay you back</h2>"
+                st.markdown(html_str_output, unsafe_allow_html=True)
+                st.markdown("<h10 style='text-align: center; color: black;'> </h10>", unsafe_allow_html=True)
+                st.markdown("<h5 style='text-align: center; color: black;'>With probability of:</h5>", unsafe_allow_html=True)
+                html_str_proba = f"<h1 style='text-align: center; color: green;'>{float(predictions['probability'])*100} %</h1>"
+                st.markdown(html_str_proba, unsafe_allow_html=True)
+                col1,col2,col3 = st.columns([1,3,1])
+                col2.write('-------------')
+                col2.image(pay_shap, use_column_width=True)
+                col2.write('-------------')
+                shap.make_shap_plot(uploaded_csv_file)
+                st.pyplot(bbox_inches='tight')
+                plt.clf()
 
 
 col1,col2,col3 = st.columns([1,1,1])
@@ -155,10 +176,10 @@ team_img = Image.open('team_imgs/team.jpg')
 # sjoerd_img = Image.open('team_imgs/sjoerd.jpeg')
 # lewagon_img = Image.open('team_imgs/lewagon.png')
 
-
-
-
 col2.image(team_img, caption="", use_column_width=True)
+
+
+
 
 
 ## CUSTOM FONT EXPERIMENTS
